3911c87da0383af7bb7d0f062c4b6d967bd52c43
Yensbroo <yens.broothaers@student.arteveldehs.be>
2017-11-06 14:17:36 +0100

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,17 @@ var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+//Compile sass into CSS
+gulp.task('sass', function(){
+    return gulp.src('app/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'));
+})
+// static server & watching scss / html files
+gulp.task('serve', function(){
+    browserSync.init({
+        server: './app'
+    });
+    gulp.watch('app/scss/*.scss', ['sass']);
+
+})
